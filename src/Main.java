e91934da51aef9853512d7b664034ee8b279678e
Stanley Ye <stanleyye1996@gmail.com>
2017-11-11 12:29:01 -0800

    JDBC connector

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            System.out.println("Cannot find JDBC driver");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/test","root", "fatman123");
+
+        } catch (SQLException ex) {
+            System.out.println("Failed to connect to database");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+            return;
+        }
+
+        if (conn != null) {
+            System.out.println("Connected to db!");
+        } else {
+            System.out.println("Still failed to connect to db");
+        }
+    }
+}
