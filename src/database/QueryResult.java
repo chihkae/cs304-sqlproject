41ba814ac410d8fcbd2541a0c87a0be4bfcf70aa
Stanley Ye <stanleyye1996@gmail.com>
2017-11-14 23:19:41 -0800

    result set parsing and tests

--- src/database/QueryResult.java
+++ src/database/QueryResult.java
@@ -3,16 +3,47 @@ package database;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueryResult {
     public static Object[][] parseResultSet(ResultSet rs) throws SQLException {
         ResultSetMetaData resultSetMetaData = rs.getMetaData();
-
         int numCols = resultSetMetaData.getColumnCount();
-        for (int x = 0; x < numCols; x++) {
-            System.out.println(resultSetMetaData.getColumnName(x + 1));
+
+        List<List<Object>> listResult = new ArrayList<>();
+        List<Object> columnNames = new ArrayList<>();
+
+        addColumnNames(resultSetMetaData, columnNames, numCols);
+        addResultSetRowsToListResult(listResult, rs, numCols);
+
+        Object[][] arrayResult = listResult.stream().map(List::toArray).toArray(Object[][]::new);
+        return arrayResult;
+    }
+
+    public static void addColumnNames(ResultSetMetaData resultSetMetaData, List<Object> columnNames,
+                                      int numCols) throws SQLException {
+        for (int x = 1; x <= numCols; x++) {
+            // System.out.println(resultSetMetaData.getColumnName(x));
+            columnNames.add(resultSetMetaData.getColumnName(x));
+        }
+    }
+
+    public static void addResultSetRowsToListResult(List<List<Object>> listResult,
+                                                    ResultSet rs, int numCols) throws SQLException {
+        while (rs.next()) {
+            List<Object> row = getRowValues(rs, numCols);
+            listResult.add(row);
+        }
+    }
+
+    public static List<Object> getRowValues(ResultSet rs, int numCols) throws SQLException {
+        List<Object> listOfRowValues = new ArrayList<>();
+
+        for (int y = 1; y <= numCols; y++) {
+            listOfRowValues.add(rs.getString(y));
         }
 
-        return null;
+        return listOfRowValues;
     }
 }
--- tests/QueryResultTest.java
+++ tests/QueryResultTest.java
@@ -1,9 +1,29 @@
+import database.Query;
+import database.QueryResult;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class QueryResultTest {
     @Test
     public void test1() {
-        System.out.println("test");
+        try {
+            ResultSet rs = Query.showAllPassengers();
+            Object[][] object2dArray = QueryResult.parseResultSet(rs);
+            printOutObjects(object2dArray);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    // Helper function to print out the objects in the 2d array
+    public void printOutObjects(Object[][] object2dArray) {
+        for (Object[] objArr : object2dArray) {
+            for (Object o : objArr) {
+                System.out.println(o);
+            }
+        }
     }
 }
